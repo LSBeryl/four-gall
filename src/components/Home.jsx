01db28cda930afb6a0b355243db93963b52083bb
LSBeryl <dltjgus8098@naver.com>
2024-04-02 23:08:04 +0900

    real finissh looginng

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -80,7 +80,6 @@ export default function Home() {
 		let isReal = false
 		ourNames.forEach(v => {
 			if(name.includes(v)) isReal = true
-			console.log(name, v, name.includes(v))
 		})
 		return isReal
 	}
