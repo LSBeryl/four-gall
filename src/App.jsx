d0d324a1c8509df4bfecf5f4b115d02e7e19a52a
LSBeryl <dltjgus8098@naver.com>
2024-04-03 07:30:02 +0900

    login error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,10 @@ function Layout() {
 function App() {
   console.log("%c콘솔 창에서 이상한 짓 하지 마십시오.", "color: red; font-size: 2.5rem;")
   console.log("%c - 2421 이서현", "color: red; font-size: 2rem;")
-  localStorage.setItem('name', '')
-  localStorage.setItem('email', '')
+  if(localStorage.getItem('name') == 'null' || localStorage.getItem('email') == 'null') {
+    localStorage.setItem('name', 'null')
+    localStorage.setItem('email', 'null')
+  }
   return (
     <BrowserRouter>
       <Routes>
