fb2a49ee316148c19eab9ffda20c1fd35bfa3eb9
LSBeryl <dltjgus8098@naver.com>
2024-04-03 08:51:24 +0900

    privacy added

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
 
   return (
 		<Wrap>
       <div>개발자 연락처 <Yeah>lsberyl@dimigo.hs.kr</Yeah></div>
+      <Privacy to="https://dimi24-privacy.netlify.app">개인정보처리방침</Privacy>
 		</Wrap>
 	);
 }
@@ -15,8 +17,7 @@ const Wrap = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
-  gap: 1rem;
+  gap: 1.3rem;
   font-size: 0.8rem;
 `
 
@@ -24,4 +25,9 @@ const Yeah = styled.span`
   color: #707070;
   font-weight: 500;
   margin-left: 0.3rem;
+`
+
+const Privacy = styled(Link)`
+  text-decoration: none;
+  color: #aaa;
 `
