d029dc83f618ce9d07b5248669a9dfd2b1acbcd5
LSBeryl <dltjgus8098@naver.com>
2024-04-08 09:57:55 +0900

    민상 광고

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ import Calendar from './components/Calendar.jsx'
 import Feature from './components/Feature.jsx'
 import Music from './components/Music.jsx'
 
+import axios from 'axios'
+
 import { getDoc, doc } from 'firebase/firestore'
 import { db } from './firebase'
 
@@ -31,12 +33,9 @@ function App() {
   console.log("%c콘솔 창에서 이상한 짓 하지 마십시오.", "color: red; font-size: 2.5rem;")
   console.log("%c - 2421 이서현", "color: red; font-size: 2rem;")
 
-  const [isFour, setIsFour] = useState(false)
-  const [isSubmitted, setIsSubmitted] = useState(false)
-
   return (
     <BrowserRouter>
-      {isFour && isSubmitted || localStorage.getItem('isFour') ?
+      {localStorage.getItem('access_token') ?
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />}/>
@@ -54,14 +53,14 @@ function App() {
             KDMHS 2-4
           </Title>
           <FormDiv>
-            <input type="password" placeholder="입장 코드 입력" onChange={async (e) => {
-              const secret = doc(db, 'secret', 'secret');
-              const secretSnapshot = await getDoc(secret);
-              const secrett = secretSnapshot.data()
-              if(secrett.str == e.target.value) setIsFour(true)
-              else setIsFour(false)
-            }}/>
-            {isFour && <button onClick={() => {setIsSubmitted(true);localStorage.setItem('isFour', true)}}>Join</button>}
+            <input type="password" placeholder="입장 코드 입력" />
+            <button onClick={async (event) => {
+              axios.post('', { secret: event.target.value })
+              .then(_result => {
+                // Token Processing
+              })
+              .catch(_error => alert('로그인에 실패하였습니다.'))
+            }}>Login</button>
           </FormDiv>
         </Contain>
       </Wrap>
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,35 +1,39 @@
 import styled from 'styled-components'
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Calendar() {
 	const [what, setWhat] = useState('')
 	const [day, setDay] = useState(-1)
 	const [classTime, setClassTime] = useState(1)
 	const [date, setDate] = useState(new Date())
+
+	const [comciData, setComciData] = useState([])
+	const [schedule, setSchedule] = useState([])
+
+	useEffect(() => {
+		const axiosFetch = async () => {
+			try {
+				const response = await axios.get(`${window.location.href.includes('local') ? 'http://localhost:5050' : 'http://dimi24.vercel.app:5050'}` + '/t');
+				console.log(response.data[4][0].subject)
+				setComciData(response.data)
+			} catch (error) {
+				console.error('Error fetching timetable data:', error);
+			}
+		}
+		axiosFetch()
+	}, [])
+
+	useEffect(() => {
+		setSchedule(comciData)
+	}, [comciData])
+
 	useEffect(() => {
     const id = setInterval(() => {
       setDate(new Date());
     }, 1000);
     return (() => clearInterval(id))
   }, []);
-
-	const schedule = [
-		[
-			'응화', '성직', '수학2', '화학', '문학', '자료구조', '자율'
-		],
-		[
-			'화학', '한국사', '수학1', '중국어', '자료구조', '운동과 건강', '수학2'
-		],
-		[
-			'국사', '진로', '영어1', '화학', '응화', '동아리', '무언가를 하는'
-		],
-		[
-			'자료구조', '성직', '문학', '영어1', '응화', '수학1', '한국사'
-		],
-		[
-			'수학1', '수학2', '운동과 건강', '중국어', '영어1', '문학', '진로'
-		]
-	]
 
 	useEffect(() => {
 		const d = date.getDay()
@@ -47,30 +51,30 @@ export default function Calendar() {
 				else if(m > 40 && m < 50) setWhat('개인 정비')
 				else if(m >= 50) setWhat('조회')
 			} else if(h == 9) {
-				if(m >= 0 && m < 50) setWhat(`1교시, ${schedule[d - 1][0]}`)
+				if(m >= 0 && m < 50) setWhat(`1교시, ${schedule[d - 1][0].subject}`)
 				else setWhat('쉬는')
 			} else if(h == 10) {
-				if(m >= 0 && m < 50) setWhat(`2교시, ${schedule[d - 1][1]}`)
+				if(m >= 0 && m < 50) setWhat(`2교시, ${schedule[d - 1][1].subject}`)
 				else setWhat('쉬는')
 			} else if(h == 11) {
-				if(m >= 0 && m < 50) setWhat(`3교시, ${schedule[d - 1][2]}`)
+				if(m >= 0 && m < 50) setWhat(`3교시, ${schedule[d - 1][2].subject}`)
 				else setWhat('쉬는')
 			} else if(h == 12) {
-				if(m >= 0 && m < 50) setWhat(`4교시, ${schedule[d - 1][3]}`)
+				if(m >= 0 && m < 50) setWhat(`4교시, ${schedule[d - 1][3].subject}`)
 				else if(m > 50) setWhat('점심')
 			} else if(h == 13) {
 				if(m >= 0 && m < 50) setWhat('점심')
-				else if(m >= 50) setWhat(`5교시, ${schedule[d - 1][4]}`)
+				else if(m >= 50) setWhat(`5교시, ${schedule[d - 1][4].subject}`)
 			} else if(h == 14) {
-				if(m < 40) setWhat(`5교시, ${schedule[d - 1][4]}`)
+				if(m < 40) setWhat(`5교시, ${schedule[d - 1][4].subject}`)
 				else if(m >= 40 && m < 50) setWhat('쉬는')
-				else if(m >= 50) setWhat(`6교시, ${schedule[d - 1][5]}`)
+				else if(m >= 50) setWhat(`6교시, ${schedule[d - 1][5].subject}`)
 			} else if(h == 15) {
-				if(m < 40) setWhat(`6교시, ${schedule[d - 1][5]}`)
+				if(m < 40) setWhat(`6교시, ${schedule[d - 1][5].subject}`)
 				else if(m >= 40 && m < 50) setWhat('쉬는')
-				else if(m >= 50) setWhat(`7교시, ${schedule[d - 1][6]}`)
+				else if(m >= 50) setWhat(`7교시, ${schedule[d - 1][6].subject}`)
 			} else if(h == 16) {
-				if(m < 40) setWhat(`7교시, ${schedule[d - 1][6]}`)
+				if(m < 40) setWhat(`7교시, ${schedule[d - 1][6].subject}`)
 				else if(m > 40 && m <= 55) setWhat('청소')
 				else if(m > 55) setWhat('종례')
 			} else if(h == 17) {
@@ -125,6 +129,7 @@ export default function Calendar() {
 					지금은 <span style={{color: '#ee1183'}}>{what}</span> 시간입니다.
 				</WhatShould>
 				<Table>
+					<tbody>
 					<tr>
 						<td></td>
 						<td style={{fontWeight: day == 1 ? 'bold' : ''}}>월</td>
@@ -133,164 +138,18 @@ export default function Calendar() {
 						<td style={{fontWeight: day == 4 ? 'bold' : ''}}>목</td>
 						<td style={{fontWeight: day == 5 ? 'bold' : ''}}>금</td>
 					</tr>
-					<tr>
-						<td>1교시</td>
-						<td style={{
-							fontWeight: day == 1 ? 'bold' : '',
-							color: classTime == 1 && day == 1 ? '#ee1183' : ''
-						}}>응화</td>
-						<td style={{
-							fontWeight: day == 2 ? 'bold' : '',
-							color: classTime == 1 && day == 2 ? '#ee1183' : ''
-						}}>화학</td>
-						<td style={{
-							fontWeight: day == 3 ? 'bold' : '',
-							color: classTime == 1 && day == 3 ? '#ee1183' : ''
-						}}>국사</td>
-						<td style={{
-							fontWeight: day == 4 ? 'bold' : '',
-							color: classTime == 1 && day == 4 ? '#ee1183' : ''
-						}}>자료</td>
-						<td style={{
-							fontWeight: day == 5 ? 'bold' : '',
-							color: classTime == 1 && day == 5 ? '#ee1183' : ''
-						}}>수1</td>
-					</tr>
-					<tr>
-						<td>2교시</td>
-						<td style={{
-							fontWeight: day == 1 ? 'bold' : '',
-							color: classTime == 2 && day == 1 ? '#ee1183' : ''
-						}}>성직</td>
-						<td style={{
-							fontWeight: day == 2 ? 'bold' : '',
-							color: classTime == 2 && day == 2 ? '#ee1183' : ''
-						}}>국사</td>
-						<td style={{
-							fontWeight: day == 3 ? 'bold' : '',
-							color: classTime == 2 && day == 3 ? '#ee1183' : ''
-						}}>진로</td>
-						<td style={{
-							fontWeight: day == 4 ? 'bold' : '',
-							color: classTime == 2 && day == 4 ? '#ee1183' : ''
-						}}>성직</td>
-						<td style={{
-							fontWeight: day == 5 ? 'bold' : '',
-							color: classTime == 2 && day == 5 ? '#ee1183' : ''
-						}}>수2</td>
-					</tr>
-					<tr>
-						<td>3교시</td>
-						<td style={{
-							fontWeight: day == 1 ? 'bold' : '',
-							color: classTime == 3 && day == 1 ? '#ee1183' : ''
-						}}>수2</td>
-						<td style={{
-							fontWeight: day == 2 ? 'bold' : '',
-							color: classTime == 3 && day == 2 ? '#ee1183' : ''
-						}}>수1</td>
-						<td style={{
-							fontWeight: day == 3 ? 'bold' : '',
-							color: classTime == 3 && day == 3 ? '#ee1183' : ''
-						}}>영1</td>
-						<td style={{
-							fontWeight: day == 4 ? 'bold' : '',
-							color: classTime == 3 && day == 4 ? '#ee1183' : ''
-						}}>문학</td>
-						<td style={{
-							fontWeight: day == 5 ? 'bold' : '',
-							color: classTime == 3 && day == 5 ? '#ee1183' : ''
-						}}>운동</td>
-					</tr>
-					<tr>
-						<td>4교시</td>
-						<td style={{
-							fontWeight: day == 1 ? 'bold' : '',
-							color: classTime == 4 && day == 1 ? '#ee1183' : ''
-						}}>화학</td>
-						<td style={{
-							fontWeight: day == 2 ? 'bold' : '',
-							color: classTime == 4 && day == 2 ? '#ee1183' : ''
-						}}>중국</td>
-						<td style={{
-							fontWeight: day == 3 ? 'bold' : '',
-							color: classTime == 4 && day == 3 ? '#ee1183' : ''
-						}}>화학</td>
-						<td style={{
-							fontWeight: day == 4 ? 'bold' : '',
-							color: classTime == 4 && day == 4 ? '#ee1183' : ''
-						}}>영1</td>
-						<td style={{
-							fontWeight: day == 5 ? 'bold' : '',
-							color: classTime == 4 && day == 5 ? '#ee1183' : ''
-						}}>중국</td>
-					</tr>
-					<tr>
-						<td>5교시</td>
-						<td style={{
-							fontWeight: day == 1 ? 'bold' : '',
-							color: classTime == 5 && day == 1 ? '#ee1183' : ''
-						}}>문학</td>
-						<td style={{
-							fontWeight: day == 2 ? 'bold' : '',
-							color: classTime == 5 && day == 2 ? '#ee1183' : ''
-						}}>자료</td>
-						<td style={{
-							fontWeight: day == 3 ? 'bold' : '',
-							color: classTime == 5 && day == 3 ? '#ee1183' : ''
-						}}>응화</td>
-						<td style={{
-							fontWeight: day == 4 ? 'bold' : '',
-							color: classTime == 5 && day == 4 ? '#ee1183' : ''
-						}}>응화</td>
-						<td style={{
-							fontWeight: day == 5 ? 'bold' : '',
-							color: classTime == 5 && day == 5 ? '#ee1183' : ''
-						}}>영1</td>
-					</tr>
-					<tr>
-						<td>6교시</td>
-						<td style={{
-							fontWeight: day == 1 ? 'bold' : '',
-							color: classTime == 6 && day == 1 ? '#ee1183' : ''
-						}}>자료</td>
-						<td style={{
-							fontWeight: day == 2 ? 'bold' : '',
-							color: classTime == 6 && day == 2 ? '#ee1183' : ''
-						}}>운동</td>
-						<td style={{
-							fontWeight: day == 3 ? 'bold' : '',
-							color: classTime == 6 && day == 3 ? '#ee1183' : ''
-						}}>CA</td>
-						<td style={{
-							fontWeight: day == 4 ? 'bold' : '',
-							color: classTime == 6 && day == 4 ? '#ee1183' : ''
-						}}>수1</td>
-						<td style={{
-							fontWeight: day == 5 ? 'bold' : '',
-							color: classTime == 6 && day == 5 ? '#ee1183' : ''
-						}}>문학</td>
-					</tr>
-					<tr>
-						<td>7교시</td>
-						<td style={{
-							fontWeight: day == 1 ? 'bold' : '',
-							color: classTime == 7 && day == 1 ? '#ee1183' : ''
-						}}>자율</td>
-						<td style={{
-							fontWeight: day == 2 ? 'bold' : '',
-							color: classTime == 7 && day == 2 ? '#ee1183' : ''
-						}}>수2</td>
-						<td></td>
-						<td style={{
-							fontWeight: day == 4 ? 'bold' : '',
-							color: classTime == 7 && day == 4 ? '#ee1183' : ''
-						}}>국사</td>
-						<td style={{
-							fontWeight: day == 5 ? 'bold' : '',
-							color: classTime == 7 && day == 5 ? '#ee1183' : ''
-						}}>진로</td>
-					</tr>
+					{schedule[0] && [1, 2, 3, 4, 5, 6, 7].map(time => ( //1, 2, 3, 4, 5, 6, 7 > n교시
+						<tr key={time}>
+							<td>{time}교시</td>
+							{[0, 1, 2, 3, 4].map((curDay) => ( // 0, 1, 2, 3, 4 > 월~금
+								<td key={curDay} style={{
+									fontWeight: day == curDay + 1 ? 'bold' : '',
+									color: classTime == time && day == curDay + 1 ? '#ee1183' : ''
+								}}>{schedule[curDay][time - 1].subject}</td>
+							))}
+						</tr>
+					))}
+					</tbody>
 				</Table>
 			</Wrap>
 		</>
