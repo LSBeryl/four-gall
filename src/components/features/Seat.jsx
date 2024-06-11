fc673ade3084ee071dcd1dd6f1bd0a500657ed89
LSBeryl <dltjgus8098@naver.com>
2024-06-11 18:27:43 +0900

    장표 업뎃

--- src/components/features/Seat.jsx
+++ src/components/features/Seat.jsx
@@ -1,31 +1,63 @@
-import styled, { keyframes } from 'styled-components'
-import { useState } from 'react'
+import styled, { keyframes } from "styled-components";
+import { useState } from "react";
 
-const ourNames = ['강민상', '고준기', '구본찬', '김도윤', '김민서', '김병규', '김승원', '김진호', '남송연', '노재원', '문석호', '박동주', '박연진', '박종윤', '손기령', '송민서', '신현솔', '윤예진', '이기원', '이기찬', '이서현', '이재영', '이준행', '이하은', '이현명', '이희탁', '조성민', '조우창', '조정환', '최희윤', '황석준']
+const ourNames = [
+  "강민상",
+  "고준기",
+  "구본찬",
+  "김도윤",
+  "김민서",
+  "김병규",
+  "김승원",
+  "김진호",
+  "남송연",
+  "노재원",
+  "문석호",
+  "박동주",
+  "박연진",
+  "박종윤",
+  "손기령",
+  "송민서",
+  "신현솔",
+  "윤예진",
+  "이기원",
+  "이기찬",
+  "이서현",
+  "이재영",
+  "이준행",
+  "이하은",
+  "이현명",
+  "이희탁",
+  "조성민",
+  "조우창",
+  "조정환",
+  "최희윤",
+  "황석준",
+];
 
 function wait(s) {
-  return new Promise(res => setTimeout(res, s * 1000))
+  return new Promise((res) => setTimeout(res, s * 1000));
 }
 
 export default function Seat() {
-  const [seat, setSeat] = useState([])
-  const [isFinish, setIsFinish] = useState(false)
+  const [seat, setSeat] = useState([]);
+  const [isFinish, setIsFinish] = useState(false);
 
   function change() {
-    const randSeat = []
-    const randNumArr = []
-    for(let i = 0; i < 31; i++) {
-      let randNum = Math.floor(Math.random() * 31 + 1) - 1
-      while(true) {
-        if(randNumArr.indexOf(randNum) == -1) break
-        randNum = Math.floor(Math.random() * 31 + 1) - 1
+    const randSeat = [];
+    const randNumArr = [];
+    for (let i = 0; i < 31; i++) {
+      let randNum = Math.floor(Math.random() * 31 + 1) - 1;
+      while (true) {
+        if (randNumArr.indexOf(randNum) == -1) break;
+        randNum = Math.floor(Math.random() * 31 + 1) - 1;
       }
-      randNumArr.push(randNum)
+      randNumArr.push(randNum);
     }
-    randNumArr.forEach(v => {
-      randSeat.push(ourNames[v])
-    })
-    setSeat([...randSeat])
+    randNumArr.forEach((v) => {
+      randSeat.push(ourNames[v]);
+    });
+    setSeat([...randSeat]);
   }
 
   return (
@@ -76,7 +108,7 @@ export default function Seat() {
           </Tr>
           <Tr>
             <Td>{seat[20]}</Td>
-            <Td style={{background: '#eaeaea'}}></Td>
+            <Td style={{ background: "#eaeaea" }}></Td>
           </Tr>
         </Table>
         <Table>
@@ -102,30 +134,32 @@ export default function Seat() {
           </Tr>
         </Table>
       </div>
-      <div style={{display: isFinish ? '' : 'none'}}>자리 배치 완료!</div>
+      <div style={{ display: isFinish ? "" : "none" }}>자리 배치 완료!</div>
       <div>
-        <button onClick={async () => {
-          setIsFinish(false)
-          let i = 0.05
-          for(let j = 0; j < 50; j++) {
-            change()
-            await wait(0.05)
-          }
-          while(i <= 0.5) {
-            change()
-            i += 0.02
-            await wait(i)
-          }
-          await wait(1)
-          change()
-          setIsFinish(true)
-        }}>자리 배치</button>
+        <button
+          onClick={async () => {
+            setIsFinish(false);
+            let i = 0.05;
+            for (let j = 0; j < 50; j++) {
+              change();
+              await wait(0.05);
+            }
+            while (i <= 0.5) {
+              change();
+              i += 0.02;
+              await wait(i);
+            }
+            await wait(1);
+            change();
+            setIsFinish(true);
+          }}
+        >
+          자리 배치
+        </button>
       </div>
-      <Warn>
-        자리 배치 기능은 큰 화면에서만 제공됩니다.
-      </Warn>
+      <Warn>자리 배치 기능은 큰 화면에서만 제공됩니다.</Warn>
     </Wrap>
-  )
+  );
 }
 
 const Wrap = styled.div`
@@ -147,16 +181,16 @@ const Wrap = styled.div`
       display: none;
     }
   }
-`
+`;
 
 const Table = styled.table`
   margin-inline: 1vw;
   @media (max-width: 700px) {
     display: none;
   }
-`
+`;
 
-const Tr = styled.tr``
+const Tr = styled.tr``;
 
 const Td = styled.td`
   text-align: center;
@@ -164,11 +198,11 @@ const Td = styled.td`
   padding: 1vw 2vw;
   font-size: 1.2rem;
   border-radius: 10px;
-`
+`;
 
 const Warn = styled.div`
   @media (max-width: 700px) {
     display: block;
   }
   display: none;
-`
+`;
